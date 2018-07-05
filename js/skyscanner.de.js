3a4d4c7d32a3eafc92a8ff59418ec92a6adb22aa
Christian Robert <dev@perdian.de>
2018-07-05 10:24:22 +0200

    Complete switch to vue.js

--- js/skyscanner.de.js
+++ js/skyscanner.de.js
@@ -1,4 +1,4 @@
-flightsearcherWebsiteProviders.skyscanner = {
+flightSearchWebsiteProviders.push({
     category: "Metasearchers",
     title: "Skyscanner.de",
     openWebsite: function(flightSearchModel) {
@@ -6,7 +6,7 @@ flightsearcherWebsiteProviders.skyscanner = {
         let skyscannerUrl = "https://www.skyscanner.de/transport/d";
         for (let i=0; i < flightSearchModel.flights.length; i++) {
             skyscannerUrl += "/" + flightSearchModel.flights[i].departureAirport;
-            skyscannerUrl += "/" + flightSearchModel.flights[i].date;
+            skyscannerUrl += "/" + flightSearchModel.flights[i].departureDate;
             skyscannerUrl += "/" + flightSearchModel.flights[i].arrivalAirport;
         }
         skyscannerUrl += "?adults=" + (flightSearchModel.numberOfPassengers == null ? 1 : flightSearchModel.numberOfPassengers);
@@ -14,8 +14,4 @@ flightsearcherWebsiteProviders.skyscanner = {
         window.open(skyscannerUrl, "_blank");
 
     }
-};
-
-
-
-//https://www.skyscanner.de/transport/d/duss/2018-09-08/dtw/las/2018-09-28/duss?adults=1&children=0&adultsv2=1&childrenv2=&infants=0&cabinclass=economy&ref=home#results
+});
